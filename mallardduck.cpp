0fdad1920255338af6ea8c8b7fcb4eac22866045
CNA <sheeet6@gmail.com>
2020-09-08 13:31:29 +0700

    Mallard ducks

---
+++ mallardduck.cpp
@@ -0,0 +1,11 @@
+#include "mallardduck.h"
+
+MallardDuck::MallardDuck()
+{
+
+}
+
+void MallardDuck::display()
+{
+    cout << "Just a mallard duck\n";
+}
---
+++ mallardduck.h
@@ -0,0 +1,11 @@
+#ifndef MALLARDDUCK_H
+#define MALLARDDUCK_H
+
+
+class MallardDuck
+{
+public:
+    MallardDuck();
+};
+
+#endif // MALLARDDUCK_H
